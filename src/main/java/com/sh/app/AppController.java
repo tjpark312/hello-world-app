bbbd0638c69cc03f5b8ed26173f393805d6a7c82
Tae Jun Park <162237157+tjpark312@users.noreply.github.com>
2024-07-02 15:37:59 +0900

    Feat: Dockerfile추가, v0.1.0

--- src/main/java/com/sh/app/AppController.java
+++ src/main/java/com/sh/app/AppController.java
@@ -1,11 +1,13 @@
 package com.sh.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class AppController {
 
     @Value("${app.version}")
@@ -13,7 +15,9 @@ public class AppController {
 
     @GetMapping("/")
     @ResponseBody
+
     public String index() {
+        log.info("GET / : appVersion = {}", appVersion);
         return """
             <h1>🎃Welcome to Hello World Application🎃</h1>
             <h4>version %s</h4>
